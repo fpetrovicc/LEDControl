11a786a412e5d6a4b54982aa7a3cb68b171fbb96
fpetrovicc <filippetrovic603@gmail.com>
2021-09-15 16:10:53 +0200

    info: updated arduino code

--- LEDControl_arduino/LEDControl_arduino.ino
+++ LEDControl_arduino/LEDControl_arduino.ino
@@ -1,147 +1,125 @@
 #include <ezOutput.h>
 
 String ulaz = "";
-boolean ulazPrim = false;
 String komanda = "";
 
-boolean isConnected = false;
-unsigned long dTime = 1000;
+boolean ulazPrim = false;
 
-ezOutput led(14);
+const unsigned long dTime = 1000; // delay u ms
+const unsigned long baudRate = 9600; // baudrate za serial
+const int ledPin = 5; // pin za LED
 
-void setup() {
-  Serial.begin(9600);
+// ezOutput library
+ezOutput led(ledPin);
+
+void setup() 
+{
+  Serial.begin(baudRate); // otvaranje serial porta
+  pinMode(ledPin, OUTPUT); // podesavanje pina za output
+
+  Serial.println("[SETUP ZAVRSEN]");
 }
 
-void loop() {
-
-  if (ulazPrim)
+void loop()
+{
+  while (Serial.available())
   {
-    ulazPrim = false;
-    traziKomandu();
+    ulaz = Serial.readString(); // citanje ulaza sa serial porta
+    traziKomandu(); // ucitavanje ulaza kao komanda
 
+    // Komanda na inicijalizaciji konekcije
     if (komanda.equals("STAR"))
     {
-      led.blink(dTime, dTime, 2);
+      Serial.println("[START FUNKCIJA]");
+      ledBlinkTimed();
     }
 
+    // Zaustavljanje sijalice
     else if (komanda.equals("STOP"))
     {
+      Serial.println("[STOP FUNKCIJA]");
       led.low();
     }
 
+    // Paljenje sijalice
     else if (komanda.equals("LEDON"))
     {
       boolean LedState = led.getState();
 
       if (LedState == false)
       {
+        Serial.println("[LED ON FUNKCIJA]");
         led.high();
       }
       
     }
 
+    // Gasenje sijalice
     else if (komanda.equals("LEDOF"))
     {
       boolean LedState = led.getState();
 
       if (LedState == true)
       {
+        Serial.println("[LED OFF FUNKCIJA]");
         led.low();
       }
     }
 
+    // Blinkanje bez prestanka
     else if (komanda.equals("LEDBL"))
     {
       boolean LedState = led.getState();
 
       if (LedState == false)
       {
-        led.blink(dTime, dTime);
+        Serial.println("[LED BLINK FUNKCIJA]");
+        ledBlink();
       }
       
     }
 
+    // Blinkanje tri puta
     else if (komanda.equals("LEDB3"))
     {
       boolean LedState = led.getState();
 
       if (LedState == false)
       {
-        led.blink(dTime, dTime, 3);
+        Serial.println("[LED BLINK x3 FUNKCIJA]");
+        ledBlinkTimed();
       }
     }
   }
 
   ulaz = "";
+
 }
 
+// Prosledjivanje ulaza kao komanda
 void traziKomandu()
 {
   if (ulaz.length() > 0)
   {
-    komanda = ulaz.substring(1, 5);
+    komanda = ulaz;
+    Serial.println("[KOMANDA: " + komanda + "]");
   }
 }
-/*
-// Kontrola LED lampica
-boolean getLedState()
+
+/* Funkcije za blinkanje LED lampica */
+void ledBlinkTimed()
 {
-  boolean state = false;
-
-  if (ulaz.substring(5,7).equals("ON"))
-  {
-    state = true;
-  }
-
-  else 
-  {
-    state = false;
-  }
-
-  return state;
+  // TO-DO - dodati blinkanje sa odredjenim tajmingom
 }
 
-void ledBlinkTimed(int count)
-{
-  for (int i = 0; i < count; i++)
-  {
-    ledOn(ledPin);
-    delay(dTime);
-    ledOff(ledPin);
-  }
-}
-
+// Privremena test funkcija
 void ledBlink()
 {
-  while (true)
-  {
-    ledOn(ledPin);
-    delay(dTime);
-    ledOff(ledPin);
-  }
-}
-
-void ledOn(int pin)
-{
-  digitalWrite(pin, HIGH);
-}
-
-void ledOff(int pin)
-{
-  digitalWrite(pin, LOW);
-}
-*/
-void serialEvent()
-{
-  while (Serial.available())
-  {
-    char inChar = (char)Serial.read();
-    ulaz += inChar;
-
-    if (inChar == '\n')
-    {
-      ulazPrim = true;
-    }
-  }
+  led.high();
+  delay(1000);
+  led.low();
+  delay(1000);
+  led.high();
+  delay(1000);
+  led.low();
 }
